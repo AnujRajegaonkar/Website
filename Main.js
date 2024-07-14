487404f6659b0a885914e1097aee587bb4a3daa8
AnujRajegaonkar <141954409+AnujRajegaonkar@users.noreply.github.com>
2024-07-15 01:55:55 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
